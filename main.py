adc3497d93987d2d84e44ee9e153753afde2c8f2
Satyam Yadav <satyamyadav@Satyams-MacBook-Air.local>
2026-05-24 17:18:00 +0530

    Added homepage route

--- main.py
+++ main.py
@@ -21,3 +21,8 @@ def predict(data: House):
     return {
         "predicted_price": float(prediction[0])
     }
+@app.get("/")
+def home():
+    return {
+        "message":"House Price Prediction API is running"
+    }
